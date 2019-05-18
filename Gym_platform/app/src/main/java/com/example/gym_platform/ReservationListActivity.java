9ef53726dde84840f260b3177d8d7f43ecefe985
최현석 <43382559+hschoi1104@users.noreply.github.com>
2019-05-19 02:54:35 +0900

    Update ReservationListActivity.java
    사용자 예약현황만 보이게 수정

--- Gym_platform/app/src/main/java/com/example/gym_platform/ReservationListActivity.java
+++ Gym_platform/app/src/main/java/com/example/gym_platform/ReservationListActivity.java
@@ -4,16 +4,21 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
 public class ReservationListActivity extends AppCompatActivity {
+
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference ReservationRef = db.collection("Reservation");
-
+    private FirebaseAuth mAuth=FirebaseAuth.getInstance();;
+    private FirebaseUser currentUser=mAuth.getCurrentUser();
     private ReservationAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +28,7 @@ public class ReservationListActivity extends AppCompatActivity {
         setUpRecyclerView();
     }
     private void setUpRecyclerView(){
-        Query query = ReservationRef.orderBy("start", Query.Direction.DESCENDING);
-
+        Query query = ReservationRef.whereEqualTo("userUID",currentUser.getUid());
         FirestoreRecyclerOptions<Reservation> options = new FirestoreRecyclerOptions.Builder<Reservation>().setQuery(query,Reservation.class).build();
 
         adapter = new ReservationAdapter(options);
